581c7d4ddc764f8aecd2786e4fa1a76e54ed1072
Elias Häußler <e.haeussler@familie-redlich.de>
2024-03-06 17:30:53 +0100

    [FEATURE] Consider recover timeout in flush queue command

--- Classes/Command/FlushQueueCommand.php
+++ Classes/Command/FlushQueueCommand.php
@@ -23,8 +23,10 @@ declare(strict_types=1);
 
 namespace CPSIT\Typo3Mailqueue\Command;
 
+use CPSIT\Typo3Mailqueue\Exception;
 use CPSIT\Typo3Mailqueue\Mail;
 use Symfony\Component\Console;
+use Symfony\Component\Mailer;
 use TYPO3\CMS\Core;
 
 /**
@@ -58,6 +60,12 @@ final class FlushQueueCommand extends Console\Command\Command
             Console\Input\InputOption::VALUE_REQUIRED,
             'Maximum number of mails to send in one iteration',
         );
+        $this->addOption(
+            'recover-timeout',
+            'r',
+            Console\Input\InputOption::VALUE_REQUIRED,
+            'Timeout in seconds for recovering mails that have taken too long to send',
+        );
     }
 
     protected function initialize(Console\Input\InputInterface $input, Console\Output\OutputInterface $output): void
@@ -78,70 +86,123 @@ final class FlushQueueCommand extends Console\Command\Command
     protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output): int
     {
         $limit = $input->getOption('limit');
+        $recoverTimeout = $input->getOption('recover-timeout');
         $transport = $this->mailer->getTransport();
 
-        // Early return if unsupported mail transport is configured
-        if (!($transport instanceof Mail\Transport\QueueableTransport)) {
-            $message = sprintf(
-                'The configured mail transport "%s" is not supported. Please configure a mail spooler as mail transport.',
-                $transport::class,
-            );
+        try {
+            // Early return if unsupported mail transport is configured
+            if (!($transport instanceof Mail\Transport\QueueableTransport)) {
+                throw new Exception\CommandFailureException(
+                    sprintf(
+                        'The configured mail transport "%s" is not supported. Please configure a mail spooler as mail transport.',
+                        $transport::class,
+                    ),
+                    self::INVALID,
+                );
+            }
 
-            $this->io->error($message);
+            $mailQueue = $transport->getMailQueue()->get();
+            $numberOfMailsInQueue = count($mailQueue);
+
+            // Early return if mail queue is empty
+            if ($numberOfMailsInQueue === 0) {
+                $message = 'No mails are currently in queue.';
+
+                $this->io->success($message);
+                $this->writeJsonResult([
+                    'result' => $message,
+                ]);
+
+                return self::SUCCESS;
+            }
+
+            // Limit number of sent mails from command option or send all mails
+            if ($limit !== null) {
+                $limit = (int)$limit;
+            } else {
+                $limit = $numberOfMailsInQueue;
+            }
+
+            // Early return if invalid limit is provided
+            if ($limit < 1) {
+                throw new Exception\CommandFailureException(
+                    'Limit must be a number greater than or equal to 1.',
+                    self::INVALID,
+                );
+            }
+
+            $this->recover($recoverTimeout, $transport);
+            $this->flushQueue($limit, $mailQueue, $transport);
+        } catch (Exception\CommandFailureException $exception) {
+            $this->io->error($exception->getMessage());
             $this->writeJsonResult([
-                'error' => $message,
+                'error' => $exception->getMessage(),
             ]);
 
-            return self::INVALID;
+            return $exception->statusCode;
         }
 
-        $mailQueue = $transport->getMailQueue();
-        $numberOfMailsInQueue = count($mailQueue);
-        $realTransport = $this->mailer->getRealTransport();
+        return self::SUCCESS;
+    }
 
-        // Early return if mail queue is empty
-        if ($numberOfMailsInQueue === 0) {
-            $message = 'No mails are currently in queue.';
+    /**
+     * @throws Exception\CommandFailureException
+     */
+    private function recover(?string $recoverTimeout, Mail\Transport\QueueableTransport $transport): void
+    {
+        if ($recoverTimeout !== null) {
+            $recoverTimeout = (int)$recoverTimeout;
 
-            $this->io->success($message);
-            $this->writeJsonResult([
-                'result' => $message,
-            ]);
+            // Show warning if non-recoverable transport is used with recover timeout
+            if (!($transport instanceof Mail\Transport\RecoverableTransport)) {
+                $this->io->warning('You passed --recover-timeout to a non-recoverable mail transport.');
+            }
 
-            return self::SUCCESS;
+            // Early return if invalid recover timeout is provided
+            if ($recoverTimeout < 1) {
+                throw new Exception\CommandFailureException(
+                    'Recover timeout must be a number greater than or equal to 1.',
+                    self::INVALID,
+                );
+            }
         }
 
-        // Limit number of sent mails from command option or send all mails
-        if ($limit !== null) {
-            $limit = (int)$limit;
-        } else {
-            $limit = $numberOfMailsInQueue;
+        // Recover mails that have taken too long to send
+        if ($transport instanceof Mail\Transport\RecoverableTransport) {
+            if ($recoverTimeout !== null) {
+                $transport->recover($recoverTimeout);
+            } else {
+                $transport->recover();
+            }
         }
+    }
 
-        // Early return if invalid limit is provided
-        if ($limit < 1) {
-            $message = 'Limit must be a number greater than or equal to 1.';
-
-            $this->io->error($message);
-            $this->writeJsonResult([
-                'error' => $message,
-            ]);
-
-            return self::INVALID;
-        }
-
+    /**
+     * @param list<Mail\Queue\MailQueueItem> $mailQueueItems
+     * @throws Exception\CommandFailureException
+     */
+    private function flushQueue(int $limit, array $mailQueueItems, Mail\Transport\QueueableTransport $transport): void
+    {
         $this->io->section('Flushing mail queue');
 
+        $numberOfMailsInQueue = count($mailQueueItems);
+        $realTransport = $this->mailer->getRealTransport();
         $progressBar = $this->io->createProgressBar($limit);
 
         // Send mails from queue
-        foreach ($progressBar->iterate($mailQueue, $limit) as $i => $item) {
+        foreach ($progressBar->iterate($mailQueueItems, $limit) as $i => $item) {
             if ($i >= $limit) {
                 $progressBar->finish();
                 break;
             }
 
-            $transport->dequeue($item, $realTransport);
+            try {
+                $transport->dequeue($item, $realTransport);
+            } catch (Mailer\Exception\TransportExceptionInterface $exception) {
+                $this->io->newLine(2);
+
+                throw new Exception\CommandFailureException($exception->getMessage(), self::FAILURE);
+            }
 
             --$numberOfMailsInQueue;
         }
@@ -172,8 +233,6 @@ final class FlushQueueCommand extends Console\Command\Command
             'sent' => $limit,
             'remaining' => $numberOfMailsInQueue,
         ]);
-
-        return self::SUCCESS;
     }
 
     /**
